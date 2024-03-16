b11e8adf706ab0f346ed175ce113b6095e64c9ed
javas <sivajigsivajig703@gmail.com>
2024-03-16 11:12:11 +0000

    UI implemented

--- react-flask-app/api/api.py
+++ react-flask-app/api/api.py
@@ -1,15 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///example.db"
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+
+
+    def __str__(self):
+        return f"{self.id} {self.content}"
+
+
+def todo_serializer(todo):
+    return {
+        'id': todo.id,
+        'content': todo.content
+    }
 @app.route('/api', methods=['GET'])
 def index():
+    todo = Todo.query.all()
+    return jsonify([ *map(todo_serializer, todo)])
+
+@app.route('/api/create', methods=['POST'])
+def create():
+    data = json.loads(request.data)
+    todo = Todo(content=data.content)
+    db.session.add(todo)
+    db.session.commit()
     return {
-        'name': 'hello world'
+        "201": "todo created successfully"
     }
 
-if __name__ == '__main__':
+CORS(app)
+if __name__ == '__main__':   
     app.run(debug=True)
